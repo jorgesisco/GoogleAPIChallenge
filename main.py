2ebef2afb377008288f59781add78cb002532df6
Jorge Sisco <jorgesisco17@gmail.com>
2023-03-13 20:17:25 +0100

    Refactored the code

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from google_api import GmailApi
 
-
 if __name__ == "__main__":
-    if __name__ == "__main__":
-        api = GmailApi(token_file="token.json")
+    api = GmailApi(token_file="token.json")
 
-        #This will return a list of messages, and also create a text file which each message
-        message_search = api.search_emails(user_id="me", search_query="python")
-        print(message_search)
+    # This will return a list of messages, and also create a text file which each message
+    message_search = api.search_emails(user_id="me",
+                                       search_query="Meeting")
+    print(message_search)
 
-        # api.send_email(to="pythonchallenge.sisco@gmail.com",
-        #                from_="pythonchallenge.sisco@gmail.com",
-        #                subject="Invitation to Attend a Team Meeting",
-        #                message_content="I hope this email finds you in good health and spirits.")
+    # send_email = api.send_email(to="pythonchallenge.sisco@gmail.com",
+    #                             from_="pythonchallenge.sisco@gmail.com",
+    #                             subject="This is an email to test the python script",
+    #                             message_content="Test 2.")
