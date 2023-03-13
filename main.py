24627e2e72e1b91cd4af79e2683b9a56d06e61fb
Jorge Sisco <jorgesisco17@gmail.com>
2023-03-13 19:50:48 +0100

    From the main.py file we initialize the instance

--- main.py
+++ main.py
@@ -1,55 +1,15 @@
-from __future__ import print_function
-
-import os.path
-
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+from google_api import GmailApi
 
 
-def main():
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+if __name__ == "__main__":
+    if __name__ == "__main__":
+        api = GmailApi(token_file="token.json")
 
-    try:
-        # Call the Gmail API
-        service = build('gmail', 'v1', credentials=creds)
-        results = service.users().labels().list(userId='me').execute()
-        labels = results.get('labels', [])
+        #This will return a list of messages, and also create a text file which each message
+        message_search = api.search_emails(user_id="me", search_query="python")
+        print(message_search)
 
-        if not labels:
-            print('No labels found.')
-            return
-        print('Labels:')
-        for label in labels:
-            print(label['name'])
-
-    except HttpError as error:
-        print(f'An error occurred: {error}')
-
-
-if __name__ == '__main__':
-    main()
+        # api.send_email(to="pythonchallenge.sisco@gmail.com",
+        #                from_="pythonchallenge.sisco@gmail.com",
+        #                subject="Invitation to Attend a Team Meeting",
+        #                message_content="I hope this email finds you in good health and spirits.")
